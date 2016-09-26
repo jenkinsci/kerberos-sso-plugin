1802a62881cfb1bb892fa2642f06a4e2b0cce1ce
Oliver Gondža <ogondza@gmail.com>
2016-09-26 13:01:48 +0200

    [JENKINS-38468] Reproduce in unittest

--- src/test/java/com/sonymobile/jenkins/plugins/kerberossso/KerberosConfigTest.java
+++ src/test/java/com/sonymobile/jenkins/plugins/kerberossso/KerberosConfigTest.java
@@ -24,8 +24,6 @@
 
 package com.sonymobile.jenkins.plugins.kerberossso;
 
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -34,9 +32,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
-import java.io.File;
-import java.util.ArrayList;
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -46,11 +42,6 @@ import static org.junit.Assert.assertTrue;
  * @author Joakim Ahle &lt;joakim.ahle@sonyericsson.com&gt;
  */
 public class KerberosConfigTest {
-
-    /**
-     * Time limit in seconds for timed tests.
-     */
-    public static final int TIME_LIMIT = 10;
 
     /**
      * Jenkins rule instance.
@@ -72,51 +63,36 @@ public class KerberosConfigTest {
     }
 
     /**
-     * Tests if Kebreros SSO plugin has a section on the global config page.
-     */
-    @Test
-    public void testKerberosHasConfigPage() {
-        HtmlElement enabled = currentPage.getElementByName("_.enabled");
-        assertNotNull("Kerberos configuration page missing.", enabled);
-
-    }
-
-    /**
      * Tests if Kerberos SSO plugin block can be expanded.
      */
     @Test
     public void testEnableKerberos() {
         HtmlElement enabled = currentPage.getElementByName("_.enabled");
+        assertNotNull("Kerberos configuration page missing.", enabled);
         enabled.fireEvent("click");
         assertNotNull("Optional block wasn't expanded.", currentPage.getElementByName("_.redirectEnabled"));
     }
 
     /**
      * Tests if the PluginImpl class changes attributes if a new config is submitted.
-     * @throws Exception if something goes wrong
      */
     @Test
     public void testIfConfigCanBeUpdated() throws Exception {
         assertFalse("Plugin already enabled", PluginImpl.getInstance().getEnabled());
 
-        File file = File.createTempFile("login", "conf");
+        String loginConf = getClass().getResource("login.conf").getFile();
 
         HtmlForm form = currentPage.getFormByName("config");
         assertNotNull(form);
 
-        form.getInputByName("_.loginLocation").setValueAttribute(file.getAbsolutePath());
+        form.getInputByName("_.loginLocation").setValueAttribute(loginConf);
         form.getInputByName("_.enabled").click();
 
-        try {
-            ArrayList<HtmlElement> elements = (ArrayList<HtmlElement>)form.getHtmlElementsByTagName("button");
-            HtmlButton button = (HtmlButton)elements.get(elements.size() - 1);
-            form.submit(button);
-            // CS IGNORE EmptyBlock FOR NEXT 3 LINES. REASON: Mocks Tests.
-        } catch (FailingHttpStatusCodeException e) {
-            // Expected since filter cannot be added to Jenkins rule.
-        }
+        rule.submit(form);
 
-        boolean wasEnabled = PluginImpl.getInstance().getEnabled();
+        PluginImpl plugin = PluginImpl.getInstance();
+        boolean wasEnabled = plugin.getEnabled();
         assertTrue("Plugin wasn't enabled after saving the new config", wasEnabled);
+        assertEquals(loginConf, plugin.getLoginLocation());
     }
 }
