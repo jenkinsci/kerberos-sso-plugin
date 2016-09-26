f246c430ebdf62cbd9139bc9dc91583ad3942d03
Oliver Gondža <ogondza@gmail.com>
2016-09-26 13:40:44 +0200

    Unused import

--- src/test/java/com/sonymobile/jenkins/plugins/kerberossso/KerberosConfigTest.java
+++ src/test/java/com/sonymobile/jenkins/plugins/kerberossso/KerberosConfigTest.java
@@ -33,7 +33,6 @@ import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
